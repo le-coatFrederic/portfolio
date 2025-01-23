7c9f8c1f7db704ffc5afb35b9ac0cef6e3fb872c
le coat fred <lecoatfred@gmail.com>
2025-01-23 01:54:49 +0100

    Domaine terminé

--- src/main/java/online/fredinfo/portfolio/domain/usecases/ProjectCRUD/SaveProjectUseCase.java
+++ src/main/java/online/fredinfo/portfolio/domain/usecases/ProjectCRUD/SaveProjectUseCase.java
@@ -1,5 +1,20 @@
 package online.fredinfo.portfolio.domain.usecases.ProjectCRUD;
 
+import online.fredinfo.portfolio.domain.entities.Project;
+import online.fredinfo.portfolio.domain.entities.ProjectRepository;
+
 public class SaveProjectUseCase {
-    
+    private final ProjectRepository projectRepository;
+
+    public SaveProjectUseCase(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public ProjectRepository getProjectRepository() {
+        return projectRepository;
+    }
+
+    public Project execute(Project project) {
+        return projectRepository.save(project);
+    }
 }
