3c77d023b24660becc1337d3ba920313741dafdb
le-coat <lecoatfred@gmail.com>
2025-06-29 18:49:55 +0200

    Début de la page présentation et des compétences

--- front/src/app/pages/identity/identity.ts
+++ front/src/app/pages/identity/identity.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-identity',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './identity.html',
   styleUrl: './identity.css'
 })
 export class Identity {
-
+    public age : number = Math.floor((Date.now() - Date.UTC(2000,8,1)) / (1000 * 60 * 60 * 24 * 365));
+    protected readonly Date = Date;
 }
