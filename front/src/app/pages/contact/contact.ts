447832d969e2268f5b3a7d9992e0d03c3794769a
le-coat <lecoatfred@gmail.com>
2025-07-01 11:10:59 +0200

    page de contact

--- front/src/app/pages/contact/contact.ts
+++ front/src/app/pages/contact/contact.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {ContactRepository} from '../../services/repositories/contact';
+import {Contact} from '../../models/contact';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
-export class Contact {
+export class ContactPage implements OnInit {
+  contacts!: Contact[];
+  private contactRepository: ContactRepository = inject(ContactRepository);
+  constructor() {
+  }
 
+  ngOnInit(): void {
+    this.contactRepository.getContacts().subscribe({
+      next: value => {
+        this.contacts = value;
+        console.log("Loaded contact " + this.contacts.length);
+      },
+      error: err => {
+        console.error("Error loading contacts : " + err);
+      }
+    });
+  }
 }
